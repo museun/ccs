ef9e796742770f54e2310c29c7f33bacb95dcf51
museun <museun@users.noreply.github.com>
2025-01-01 00:57:19 -0500

    Fix span relocation  (#14)
    * fit span relocation
    
    previously when it encounters a grapheme cluster
    it should find the floor/ceil position of the
    continuation byte to index into the span string
    
    this uses a copy of two unstable functions from std:
    `str::floor_char_boundary`
    `str::ceil_char_boundary`
    which are tracked here: https://github.com/rust-lang/rust/issues/93743
    
    when this becomes stable, we can just rely on std.
    but for now they are fine. (even if they are
    obviously inefficient)
    
    * bump version

--- src/parse/text.rs
+++ src/parse/text.rs
@@ -1,6 +1,6 @@
 #[derive(Debug, serde::Deserialize)]
 pub struct Text {
-    pub highlight_start: usize,
-    pub highlight_end: usize,
+    pub highlight_start: usize, // are these byte or grapheme indices?
+    pub highlight_end: usize,   // are these byte or grapheme indices?
     pub text: String,
 }
