ef1b6c3d422baf54929a1f1b1ad40c2adaebd15a
museun <museun@users.noreply.github.com>
2025-01-01 00:53:43 -0500

    fit span relocation
    previously when it encounters a grapheme cluster
    it should find the floor/ceil position of the
    continuation byte to index into the span string
    
    this uses a copy of two unstable functions from std:
    `str::floor_char_boundary`
    `str::ceil_char_boundary`
    which are tracked here: https://github.com/rust-lang/rust/issues/93743
    
    when this becomes stable, we can just rely on std.
    but for now they are fine. (even if they are
    obviously inefficient)

--- src/parse/span.rs
+++ src/parse/span.rs
@@ -22,9 +22,13 @@ impl Span {
         if matches!(render_options.render, RenderStyle::Full) {
             use owo_colors::OwoColorize as _;
             self.relocate().try_for_each(|(start, end, text)| {
+                let start = floor_char_boundary(text, start);
+                let end = ceil_char_boundary(text, end);
+
                 let head = &text[..start];
                 let mid = &text[start..end];
                 let tail = &text[end..];
+
                 writeln!(
                     out,
                     "  {head}{mid}{tail}",
@@ -68,14 +72,56 @@ impl Span {
                     left_pad = span.text.len() - s.len();
                 }
 
+                let start = span.highlight_start.saturating_sub(left_pad + 1);
+                let end = span.highlight_end.saturating_sub(left_pad + 1);
+
+                let start = str_indices::chars::from_byte_idx(&span.text, start);
+                let end = str_indices::chars::from_byte_idx(&span.text, end);
+
                 // error messages are 1 indexed
                 break Some((
-                    span.highlight_start.saturating_sub(left_pad + 1),
-                    span.highlight_end.saturating_sub(left_pad + 1),
+                    start,
+                    end,
                     // TODO use unicode-segmentation here
+                    // what does this mean? how would segmentation be applicable here?
                     &span.text[left_pad..],
                 ));
             }
         })
     }
+}
+
+// NOTE this is taken from <https://github.com/rust-lang/rust/issues/93743>
+// TODO its currently unstable but its fine for what we need
+fn floor_char_boundary(str: &str, index: usize) -> usize {
+    if index >= str.len() {
+        str.len()
+    } else {
+        let lower_bound = index.saturating_sub(3);
+        let new_index = str.as_bytes()[lower_bound..=index]
+            .iter()
+            .rposition(|&b| is_utf8_char_boundary(b));
+
+        lower_bound + new_index.unwrap()
+    }
+}
+
+// NOTE this is taken from <https://github.com/rust-lang/rust/issues/93743>
+// TODO its currently unstable but its fine for what we need
+fn ceil_char_boundary(str: &str, index: usize) -> usize {
+    if index > str.len() {
+        str.len()
+    } else {
+        let upper_bound = Ord::min(index + 4, str.len());
+        str.as_bytes()[index..upper_bound]
+            .iter()
+            .position(|&b| is_utf8_char_boundary(b))
+            .map_or(upper_bound, |pos| pos + index)
+    }
+}
+
+// NOTE impl detail of `u8::is_utf8_char_boundary` used by `floor_char_boundary` and `ceil_char_boundary`
+const fn is_utf8_char_boundary(byte: u8) -> bool {
+    // This is bit magic equivalent to: b < 128 || b >= 192
+    (byte as i8) >= -0x40
 }
